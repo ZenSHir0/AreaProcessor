422f5e381c887a0cfbfa804a6f4c71ee4941ec2d
zenshiro <pavelochek@outlook.com>
2024-03-29 00:14:21 +0300

    Add project files.

---
+++ AreaProcessor/GeometryProcessing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AreaProcessor;
+
+public static class GeometryProcessing
+{
+    public static double CircleArea(double radius)
+    {
+        if (radius <= 0)
+            throw new ArgumentException("Radius must be lower than zero number");
+
+        return Math.Pow(radius, 2) * Math.PI;
+    }
+    public static double TriangleArea(double a, double b, double c)
+    {
+        if (a <= 0 || b <= 0 || c <= 0)
+            throw new ArgumentException("One or more sides of a triangle are equal to or less than zero");
+        if (a + b <= c || a + c <= b || b + c <= a)
+            throw new ArgumentException("The sum of two side lengths has to exceed the length of the third side");
+
+        double perimeter = a + b + c;
+        double semiperimeter = perimeter / 2;
+        double result = Math.Sqrt(semiperimeter * (semiperimeter - a)
+            * (semiperimeter - b) * (semiperimeter - c));
+
+        return result;
+    }
+
+    public static bool IsTriangleRight(double a, double b, double c)
+    {
+        if (a <= 0 || b <= 0 || c <= 0)
+            throw new ArgumentException("One or more sides of a triangle are equal to or less than zero");
+
+        double[] sides = [a, b, c];
+        return Math.Pow(sides[2], 2) == Math.Pow(sides[1], 2) + Math.Pow(sides[0], 2);
+    }
+}
---
+++ TestProject1/CalculateAreaTests.cs
@@ -0,0 +1,102 @@
+using AreaProcessor;
+
+namespace TestProject1;
+
+[TestClass]
+public class CalculateAreaTests
+{
+    [TestMethod]
+    [DataRow(1)]
+    [DataRow(99.9)]
+    [DataRow(150)]
+    public void CalculateCircleArea_ValidRadiusValue_ReturnCorrectArea(double value)
+    {
+        double radius = value;
+
+        double expectedArea = radius * radius * 3.1415926535897931;
+
+        double testArea = GeometryProcessing.CircleArea(radius);
+
+        Assert.AreEqual(expectedArea, testArea);
+    }
+
+    [TestMethod]
+    [DataRow(-1)]
+    [DataRow(-10)]
+    [DataRow(-463.2)]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CalculateCircleArea_NegativeRadiusValue_ReturnArgumentException(double value)
+    {
+        double radius = value;
+
+        GeometryProcessing.CircleArea(radius);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CalculateCircleArea_ZeroRadius_ReturnArgumentException()
+    {
+        double radius = 0;
+
+        GeometryProcessing.CircleArea(radius);
+    }
+
+    [TestMethod]
+    [DataRow(4, 3, 5)]
+    [DataRow(19.2, 22, 26)]
+    [DataRow(1, 1, 1)]
+    public void CalculateTriangleArea_ValidSideValues_ReturnCorrectArea(double value1, double value2, double value3)
+    {
+        double a = value1;
+        double b = value2;
+        double c = value3;
+        double semiperimeter = (a + b + c) / 2;
+        double excepted = Math.Sqrt(semiperimeter * (semiperimeter - a)
+            * (semiperimeter - b) * (semiperimeter - c));
+
+        var actual = GeometryProcessing.TriangleArea(a, b, c);
+
+        Assert.AreEqual(actual, excepted);
+    }
+
+    [TestMethod]
+    [DataRow(0, 2, 3)]
+    [DataRow(0, 0, 0)]
+    [DataRow(9.22, 0, 3)]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CalculateTriangleArea_ZeroSideValue_ReturnArgumentException(double value1, double value2, double value3)
+    {
+        GeometryProcessing.TriangleArea(value1, value2, value3);
+    }
+
+    [TestMethod]
+    [DataRow(5, 2, 1)]
+    [DataRow(1, 2, 1)]
+    [DataRow(55, 0, 3)]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CalculateTriangleArea_InvalidSideValue_ReturnArgumentException(double value1, double value2, double value3)
+    {
+        GeometryProcessing.TriangleArea(value1, value2, value3);
+    }
+
+    [TestMethod]
+    [DataRow(3, 4, 5)]
+    [DataRow(5, 12, 13)]
+    [DataRow(7, 24, 25)]
+    public void IsTriangleRight_ValidSideValues_ReturnTrue(double value1, double value2, double value3)
+    {
+        var actual = GeometryProcessing.IsTriangleRight(value1, value2, value3);
+
+        Assert.AreEqual(actual, true);
+    }
+
+    [TestMethod]
+    [DataRow(0, -3, 1)]
+    [DataRow(10.2, 0, 2)]
+    [DataRow(9, 1, 0)]
+    [ExpectedException(typeof(ArgumentException))]
+    public void IsTriangleRight_ZeroSideValues_ReturnArgumentException(double value1, double value2, double value3)
+    {
+        GeometryProcessing.IsTriangleRight(value1, value2, value3);
+    }
+}
